f3ccf59490ecb98585dd58295ec18f54edd16a8c
Simon Gubitz <simonjonasg@gmail.com>
2025-10-12 22:43:22 +0200

    Remove debug output

--- projects/sorting-algorithms/src/algorithms/selection-sort.asm
+++ projects/sorting-algorithms/src/algorithms/selection-sort.asm
@@ -50,64 +50,9 @@ _selection_sort:
 
     mov rdx, rbx        ; min = j
 
-    push rsi    ; temp storage for rdi access counter
-    push rcx
-    push r8
-    push rdi
-    push rax
-    push rbx
-
-    push rdx
-    ; print out the message
-    mov rsi, setting_msg
-    mov rdx, setting_msg_len
-    call _write
-
-    pop rdx
-    push rdx
-    mov r8, rdx
-    mov rsi, itoa_buff
-    call _itoa
-
-    mov rsi, itoa_buff
-    mov rdx, rdi
-    call _write
-
-    mov rsi, newln
-    mov rdx, 1
-    call _write
-
-    pop rdx
-
-    pop rbx
-    pop rax
-    pop rdi
-    pop r8
-    pop rcx
-    pop rsi
-
     jmp .continue
 
 .skip_update_min:
-    push rsi
-    push rcx
-    push r8
-    push rdx
-    push rdi
-    push rax
-    push rbx
-
-    mov rsi, skip_msg
-    mov rdx, skip_msg_len
-    call _write
-
-    pop rbx
-    pop rax
-    pop rdi
-    pop rdx
-    pop r8
-    pop rcx
-    pop rsi
 .continue:
 
     inc rbx
