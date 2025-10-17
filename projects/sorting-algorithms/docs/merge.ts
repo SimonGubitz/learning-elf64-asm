c426e375c76ade1c4e72e64a8a558221e0f2743c
Simon Gubitz <simonjonasg@gmail.com>
2025-10-17 23:05:06 +0200

    WIP

--- projects/sorting-algorithms/docs/merge.ts
+++ projects/sorting-algorithms/docs/merge.ts
@@ -21,7 +21,7 @@ let rcx: number;
 let rsi: memory_address;
 
 /**
- *
+ * compare length register
  */
 let rbx: number;
 
@@ -36,7 +36,7 @@ let rsp: number;
 let rdi: number;
 
 /**
- *
+ * temporary stash for previous array length
  */
 let r8: number;
 
@@ -90,8 +90,9 @@ const arr = new Int32Array(arr_buff);  // 32 bit (4 byte) integer array
 const mmap: (bytes: number) => number = (bytes: number) => {
     let current_bytes = arr_buff.byteLength;
     arr_buff.resize(current_bytes + bytes);
-    console.log(`Increase buffer size from ${current_bytes} to ${arr_buff.byteLength}.\n`)
-    return rdx;
+    console.log(`Increase buffer size from ${current_bytes}b to ${arr_buff.byteLength}b.`)
+    console.log(`Increase buffer size from ${current_bytes / 4} elements to ${arr_buff.byteLength / 4} elements.\n`)
+    return arr_buff.byteLength;
 };
 const munmap: () => number = () => {
     // rdx was poped before
@@ -111,65 +112,94 @@ const fillArr = () => {
  * @param rsi in this case the index in the array, in assembly the real address
  * @param rdx the elements in the array, in assembly the 1/4 offset in the memory due to 4 byte sized int elements
  */
-function _mergesort() {
+function AsmMergesort() {
 
     const init: () => void = () => {
-        rsi = 0;            // xor rcx, rcx
         // reserve more space
         rax = mmap(4 * rdx);    // mmap returns the address of the new space in rax
     };
 
     init();
 
-    console.log('in mergesort with addr: ', rsi, ' and rdx: ', rdx);
-    console.log(arr.subarray(rsi, rdx));
-    if (rsi === rdx) {
-        stack.push(rsi);    // push rsi
-        stack.push(rdx);    // push rdx
-        return;             // ret
-    }
+    /**
+     * the main sorting function, seperated due to idiomatics
+     */
+    const _mergesort: () => void = () => {
+        console.log('in mergesort with addr: ', rsi, ' and rdx: ', rdx);
+        console.log(arr.subarray(rsi, rdx));
 
-    stack.push(rsi);        // push rsi
-    stack.push(rdx);        // push rdx
+        rbx = rdx;      // mov rbx, rdx
+        rbx -= rsi;     // sub rbx, rsi
+        if (rbx <= 1) {         // cmp rbx 1, jle .return jmp, .skip
+            //.return:
+            stack.push(rsi);    // push rsi
+            stack.push(rdx);    // push rdx
+            return;             // ret
+        }
+        // .skip:
 
-    // left
-    // rax still holds the address of the new memory block
-    rsi = rax;              // mov rsi, rax
-    // get the middlepoint
-    rax = rdx;
-    rcx = 2;
-    rdx = rax % rcx;
-    rax = Math.floor(rax / rcx);    // idiv rcx
-    rdx += rax;                     // add rdx, rax
-                                    // now the bigger side is left
+        stack.push(rsi);        // push rsi
+        stack.push(rdx);        // push rdx
 
-    console.log('left: ', rdx);   // middle -> last element of left
-    _mergesort();
+        /**
+         * @param rsi start address
+         * @param rdx length
+         */
+        const fill_arr: (source?: string) => void = (source: string = '') => {
+            stack.push(rsi);
+            stack.push(rdx);
 
-
-    // right
-    rsi = rdx;
-    rsi += 1;   // one more than the last of left
-    rdx = stack.pop();
-    stack.push(rdx);    // the length of the original array
-    console.log('right: ', rdx);
-    _mergesort();
+            // starting at rdx for rax times
 
 
-    const fill_arr: () => void = () => {
+            console.log('rsi: ', rsi);
+            console.log('rdx: ', rdx);
+            console.log(`filled ${source} with ${arr.subarray(rsi, rdx)}`)
 
-    }
+            rdx = stack.pop();
+            rsi = stack.pop();
+            return
+        }
+
+        // left
+        // rax still holds the address of the new memory block
+        // rsi = rax;              // mov rsi, rax
+        // get the middlepoint
+        rax = rdx;
+        rcx = 2;
+        rdx = rax % rcx;
+        rax = Math.floor(rax / rcx);    // idiv rcx
+        rdx += rax;                     // add rdx, rax
+                                        // now the bigger side is left
+                                        // rax now also holds the length of the rest
+
+        console.log('middle: ', rdx);   // middle -> last element of left
+        fill_arr('left');
+        _mergesort();
+
+
+        // right
+        rsi = rdx;
+        rsi += 1;   // one more than the last of left
+        rdx = stack.pop();
+        stack.push(rdx);    // the length of the original array
+        console.log('middle: ', rdx);
+        fill_arr('right');
+        _mergesort();
 
 
 
-    const merge: () => void = () => {
+        const merge: () => void = () => {
 
+        };
+
+        merge();
     };
 
-    merge();
+    _mergesort();
 
 
-
+    // ? here or above merge call
     // pop in reverse LIFO
     rdx = stack.pop();      // pop rdx
     rsi = stack.pop();      // pop rsi
@@ -187,5 +217,5 @@ rsi = 0;
 rdx = 5;
 fillArr();
 console.log(arr);
-const merge_res = _mergesort();
+const merge_res = AsmMergesort();
 console.log(arr.subarray(0, 5))
